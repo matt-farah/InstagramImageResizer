f9cef72686703da87166e186687b958176594680
Matt Farah <114084392+matt-farah@users.noreply.github.com>
2025-07-13 09:24:07 -0700

    Add ability to add border and change canvas color

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from PIL import Image, ImageOps
 import os
 
+#set the folder path to process
 folderToProcess = "/users/matt1125/Downloads/test"
+#set the final image size
 finalHeight = 1350
 finalWidth = 1080
+#set the border size
+finalBorder = 0
+#set the background color
+finalCanvasR = 255
+finalCanvasG = 255
+finalCanvasB = 255
 
-def process_image (input, output, targetWidth, targetHeight):
+def process_image (input, output, targetWidth, targetHeight, borderSize, canvasR, canvasG, canvasB):
     # Load. the image
     image = Image.open(input).convert("RGB")
     originalWidth, originalHeight = image.size
     print ("originalw",originalWidth,"originalh", originalHeight)
     
     #resize the image
-    ratio = min(targetWidth / originalWidth, targetHeight /originalHeight)
+    ratio = min((targetWidth - borderSize )/ originalWidth, (targetHeight - borderSize) /originalHeight)
     print(ratio)
     newSize = (int(originalWidth * ratio), int (originalHeight * ratio))
     resizedImage = image.resize(newSize)
 
     #create a new white canvas and center the image
-    canvas = Image.new('RGB', (targetWidth,targetHeight), (255,255,255))
+    canvas = Image.new('RGB', (targetWidth,targetHeight), (canvasR,canvasG,canvasB))
     offset_x = (targetWidth - resizedImage.width) // 2
     offset_y = (targetHeight - resizedImage.height) // 2
     canvas.paste (resizedImage, (offset_x, offset_y))
@@ -33,7 +41,7 @@ with os.scandir(folderToProcess) as files:
         filePath = folderToProcess + "/" + name
         newFilePath = folderToProcess + "/" + "i_" + name
         print(filePath,newFilePath,finalWidth,finalHeight)
-        process_image (filePath,newFilePath,finalWidth,finalHeight)
+        process_image (filePath,newFilePath,finalWidth,finalHeight,finalBorder,finalCanvasR,finalCanvasG,finalCanvasB)
 
 
 # process_image ("/Users/matt1125/Documents/Github/InstagramImageResizer/DSCF0462.JPG","/Users/matt1125/Documents/Github/InstagramImageResizer/Resized.jpg", 1080, 1350)
